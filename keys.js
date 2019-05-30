8c7771b9a1d79c02cedd0240898bcf645ae67f1b
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-05-29 22:44:07 -0400

    inital setup complete, will get more work done tomorrow

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+  keys: fc13ddc79b2c40de8f87d86fb66ec0a4
+};
+
+exports.bandsInTown = {
+   apiKey: ;
+};
+
+exports.omdbAPi = {
+    omdbAPiKey: bd8a58bd,
+};
+
