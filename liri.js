d2da7caec9eb88820758e6537c490d19b6d1a404
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-05-30 22:46:55 -0400

    worked on spotify node calls

--- liri.js
+++ liri.js
@@ -1,3 +1,32 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+switch(command){
+    
+case "spotify-this-song":
+    showSongInfo();
+    break;
+
+case "movie-this";
+    showMovieInfo();
+    break;
+
+case "do-what-it-says":
+    doWhatItSays();
+    break;
+};
+
+function showSongInfo(){
+    var spotify = new Spotify(llaves.spotifyKeys);
+
+    spotify.search({ type: 'track', query: userInput}, function (err, data) {
+        if (err){
+            console.log('The error is ' + err);
+        }
+        console.log('Artist' + data.tracks.items[0].artists[0].name + "Song's name:" + data.tracks.items[0].name +
+        "Preview of Song:" + data.tracks.items[0].artists[0].preview_url + "Album the songs on" + data.tracks.items[0].album.name);
+    });
+};
